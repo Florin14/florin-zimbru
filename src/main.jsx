62a80e872f139db6d3509942a4cff2b55eecf5c0
Zimbru Florin <florinz@cicadatech.eu>
2024-02-18 12:46:03 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.NEXT_PUBLIC_BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
